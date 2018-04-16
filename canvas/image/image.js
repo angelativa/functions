5dacabd5e5cbd43acb90e50672a195e7580e4762
tiva <752990270@qq.com>
2018-04-16 21:05:42 +0800

    update

---
+++ canvas/image/image.js
@@ -0,0 +1,11 @@
+;(function () {
+
+    "use strict";
+
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    canvas.width = 400;
+    canvas.height = 400;
+
+})();
